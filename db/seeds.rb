dee65cc46c4d3c7fbcc38d7943e56ffbfe84be0f
Philip Vieira <philip@vallin.se>
2014-03-25 22:01:39 +0000

    Seed the database with ponies.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Environment variables (ENV['...']) can be set in the file config/application.yml.
-# See http://railsapps.github.io/rails-environment-variables.html
+Character.create([
+  { name: "Rainbow Dash",     slug: "rainbow"   },
+  { name: "Pinkie Pie",       slug: "pinkie"    },
+  { name: "Twilight Sparkle", slug: "twilight"  },
+  { name: "Apple Jack",       slug: "applejack" },
+  { name: "Fluttershy",       slug: "flutter"   },
+  { name: "Rarity",           slug: "rarity"    }
+])
