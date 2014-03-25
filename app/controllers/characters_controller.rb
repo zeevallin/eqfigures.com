180f263ac3f64c1a0a73b6dcd58ca7cbc8713d20
Philip Vieira <philip@vallin.se>
2014-03-25 20:38:27 +0000

    Some shit needs to be going on.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,20 +1,26 @@
 class CharactersController < ApplicationController
 
   def index
+    @characters = Character.all
   end
 
   def new
+    @character = Character.new
     render :form
   end
 
   def show
+    @character = Character.find params.require :id
     render :form
   end
 
   def create
+    @character = Character.new params.require :character
   end
 
   def update
+    @character = Character.find params.require :id
+    @character.write_attributes params.require :character
   end
 
 end
