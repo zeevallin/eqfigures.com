d3e78cd7dd1b4fc85ed6f4c540bd4d8dde1d37fa
Philip Vieira <philip@vallin.se>
2014-03-25 19:59:03 +0000

    rails_apps_composer: set up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325195846) do
+ActiveRecord::Schema.define(version: 20140325195852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140325195846) do
     t.string   "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role"
   end
 
 end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,6 +1,8 @@
 Fabricator(:user) do
-  name     "MyString"
-  email    "MyString"
-  provider "MyString"
-  uid      "MyString"
+  name     'Test User'
+  email    'example@example.com'
+  password 'changeme'
+  password_confirmation 'changeme'
+  # required if the Devise Confirmable module is used
+  # confirmed_at Time.now
 end
