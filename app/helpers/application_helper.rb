03a9b8b77e10e7a131b8689f40418e1cb388e3c1
Philip Vieira <philip@vallin.se>
2014-03-26 23:17:24 +0000

    Add helpers n shit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def icon_suffix(ico, text)
+    ( text + icon(ico) ).html_safe
+  end
+
+  def icon_prefix(ico, text)
+    ( icon(ico) + text ).html_safe
+  end
+
+  def icon(ico, text = "")
+    content_tag(:span, text, :class => "ico-#{ico}").html_safe
+  end
+
 end
