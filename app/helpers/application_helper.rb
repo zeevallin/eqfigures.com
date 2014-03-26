8f386e2b6a662d3fb8ae902608dca9d226fec80c
Victor Lourng <hello@victorlourng.com>
2014-03-26 19:31:57 -0400

    Space on prefix/suffix icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def icon_suffix(ico, text)
-    ( text + icon(ico) ).html_safe
+    ( text + " " + icon(ico) ).html_safe
   end
 
   def icon_prefix(ico, text)
-    ( icon(ico) + text ).html_safe
+    ( icon(ico) +  " " + text ).html_safe
   end
 
   def icon(ico, text = "")
